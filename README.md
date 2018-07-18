223a26ac91a94741796691f3364a34ef7c143f4e
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2018-07-18 06:01:26 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# sqlalchemy-commons
+# SQLAlchemy commons
+SQLAlchemy common models
+
+## Setting up an environment ##
+`python3.7 -m venv ~/.venv37`
+
+`source ~/.venv37/bin/activate`
+
+## Installing dependencies ##
+`python3.7 -m pip install --upgrade -r requirements.txt`
+
+`python3.7 -m pip install --upgrade -r requirements_dev.txt`
+
+## Testing ##
+`python3.7 setup.py test`
+
+## Building ##
+`python3.7 setup.py build`
+
+## Installing ##
+`python3.7 -m pip install --upgrade dist/sqlalchemy_commons-*.whl`
---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import os.path
+from setuptools import setup
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+
+    return [line for line in lineiter if line and not line.startswith('#')]
+
+
+install_reqs = parse_requirements('requirements.txt')
+
+current_file_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(current_file_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='sqlalchemy_commons',
+    version='0.0.1',
+    python_requires='~=3.7',
+    packages=[
+        'sqlalchemy_commons',
+    ],
+    # setup_requires=['pytest-runner'],
+    # tests_require=['pytest'],
+    package_dir={'': 'src'},
+    url='https://github.com/bixority/sqlalchemy-commons',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    keywords='sqlalchemy orm common models',
+    install_requires=install_reqs,
+    author='Olegs Korsaks',
+    author_email='kamikaze.is.waiting.you@gmail.com',
+    description='SQLAlchemy common models',
+    test_suite='tests'
+)
